243aecb470aee62838cdaaa3f454b68853682871
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-04-03 12:44:59 +0530

    [Naman Malhotra] ADD: Testing Exception Handling

--- src/test/java/com/bridgelabz/junit/basictesting/CalculatorExceptionTest.java
+++ src/test/java/com/bridgelabz/junit/basictesting/CalculatorExceptionTest.java
@@ -4,33 +4,7 @@ import com.bridgelabz.junit.basic.Calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
-
-
-    @Test
-    public void addTest(){
-        int result = Calculator.add(2,5);
-        int expectResult = 7;
-
-        Assertions.assertEquals(expectResult,result,"Test Case failed");
-    }
-
-    @Test
-    public void subtractTest(){
-        int result = Calculator.subtract(2,5);
-        int expectResult = -3;
-
-        Assertions.assertEquals(expectResult,result,"Test Case failed");
-    }
-
-    @Test
-    public void multiplyTest(){
-        int result = Calculator.multiply(2,5);
-        int expectResult = 10;
-
-        Assertions.assertEquals(expectResult,result,"Test Case failed");
-    }
-
+public class CalculatorExceptionTest {
     @Test
     public void divideTest() {
         // Case 1: Normal Division
